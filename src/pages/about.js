a6048b5b9001c3ac7cbbc581f3fb5c2d159cdd2a
konrad <konradcz02@gmail.com>
2022-03-24 19:23:38 +0100

    created Post on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainLayout from "src/components/layouts/MainLayout";
 import AboutBanner from "src/components/molecules/AboutBanner/AboutBanner";
+import AboutPost from "src/components/molecules/AboutPost/AboutPost";
 
 const About = () => {
     return (
         <MainLayout>
             <AboutBanner />
+            <AboutPost />
         </MainLayout>
     )
 }
