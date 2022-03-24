2ffae84363f4cb08b576618604efe5420fb3ba53
konrad <konradcz02@gmail.com>
2022-03-24 19:55:10 +0100

    fetched locations data from cms

---
+++ src/components/organisms/LocationsList/LocationsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LocationsList = ({data}) => {
+    return (
+        <div></div>
+    )
+}
+
+export default LocationsList
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,33 @@ import React from 'react'
 import MainLayout from "src/components/layouts/MainLayout";
 import AboutBanner from "src/components/molecules/AboutBanner/AboutBanner";
 import AboutPost from "src/components/molecules/AboutPost/AboutPost";
+import {graphql} from "gatsby";
+import LocationsList from "src/components/organisms/LocationsList/LocationsList";
 
-const About = () => {
+const About = ({data}) => {
+    const locationData = data.allDatoCmsLocation.edges;
     return (
         <MainLayout>
-            <AboutBanner />
-            <AboutPost />
+            <AboutBanner/>
+            <AboutPost/>
+            <LocationsList data={locationData} />
         </MainLayout>
     )
 }
 
 export default About
+
+export const query = graphql`
+query MyLocationQuery {
+  allDatoCmsLocation {
+    edges {
+      node {
+        name
+        icon {
+          gatsbyImageData(height: 200, forceBlurhash: false, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+}
+`;
