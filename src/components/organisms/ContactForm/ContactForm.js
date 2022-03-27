53a8c3f7635e1d5e4aded490864c1813ce800c23
konrad <konradcz02@gmail.com>
2022-03-27 17:17:59 +0200

    little changes in styles

--- src/components/organisms/ContactForm/ContactForm.js
+++ src/components/organisms/ContactForm/ContactForm.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import {Paragraph, Title} from "src/components/atoms/Titles";
 import {ContactFormWrapper} from "src/components/organisms/ContactForm/ContactForm.styles";
+import RequiredIcon from 'src/icons/required.svg'
+
+const initialState = {
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+}
+
+const reducer = (state, action) => {
+    if (action.type === 'CHANGE_VALUE') {
+        return {
+            ...state,
+            [action.payload.name]: action.payload.value
+        }
+    }
+
+    return state;
+}
 
 const ContactForm = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const handleInputChange = (name, value) => {
+        dispatch({type: 'CHANGE_VALUE', payload: {name, value}})
+    }
     return (
         <ContactFormWrapper>
             <div>
@@ -16,11 +39,23 @@ const ContactForm = () => {
             <form onSubmit={(event) => {
                 event.preventDefault();
             }}>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email Address" />
-                <input type="tel" placeholder="Phone" />
-                <textarea placeholder="Your Message" />
-                <input type="submit" value="SUBMIT" />
+                <label htmlFor="name">
+                    <input type="text" name="name" id="name" placeholder="Name"
+                           onChange={(event) => handleInputChange(event.target.name, event.target.value)}/>
+                </label>
+                <label htmlFor="email">
+                    <input type="email" name="email" id="email" required={false} placeholder="Email Address"
+                           onChange={(event) => handleInputChange(event.target.name, event.target.value)}/>
+                </label>
+                <label htmlFor="phone">
+                    <input type="tel" name="phone" id="phone" placeholder="Phone"
+                           onChange={(event) => handleInputChange(event.target.name, event.target.value)}/>
+                </label>
+                <label htmlFor="message">
+                    <textarea name="message" id="message" placeholder="Your Message"
+                              onChange={(event) => handleInputChange(event.target.name, event.target.value)}/>
+                </label>
+                <input type="submit" value="SUBMIT"/>
             </form>
         </ContactFormWrapper>
     )
